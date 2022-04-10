9bd662c5fe9b09aabc370c833a558f6f0553f3a8
FinforDev <fzwork899@gmail.com>
2022-04-10 18:15:56 -0400

    asdf

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = ({ click }) => {
 
       <div className="hamburger__menu" onClick={click}>
         <div></div>
-        <div>test3branch</div>
+        <div>test3branchhhhh</div>
         <div>gitpushtest</div>
       </div>
     </nav>
