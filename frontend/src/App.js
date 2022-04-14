9bab66cf1a88cec9b490d450199eb93f8e7f407d
FinforDev <fzwork899@gmail.com>
2022-04-14 11:30:19 -0400

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,12 +15,11 @@ import CartScreen from "./screens/CartScreen";
 function App() {
   const [sideToggle, setSideToggle] = useState(false);
 
-  console.log(sideToggle)
-  console.log("masterBranch")
-
   const test1Feature = () => {
     console.log("test10")
   }
+  console.log(sideToggle)
+  console.log("masterBranch")
   return (
     <Router>
       <Navbar click={() => setSideToggle(true)} />
