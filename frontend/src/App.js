25215f71a174e9d4a28a462ed706c832fa99da1c
FinforDev <fzwork899@gmail.com>
2022-04-14 10:43:13 -0400

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import CartScreen from "./screens/CartScreen";
 
 function App() {
   const [sideToggle, setSideToggle] = useState(false);
-  const [sideToggle, setSideToggle] = useState(false);
+ 
   console.log(sideToggle)
   console.log("masterBranch")
 
